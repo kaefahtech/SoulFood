409809cc0069455176767bd75f33369325212bb3
Kojo Amponsah Efah <kojampef4@gmail.com>
2020-06-06 05:38:09 -0400

    Added custom user model

---
+++ app/appcore/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                        PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new user
+
+        Args:
+            email (str): Email address of user
+            password (str, optional): Password of user. Defaults to None.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creats and saves a new superuser
+
+        Args:
+            email (str): Email address of superuser
+            password (str): Password of superuser
+        """
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username
+
+    Args:
+        AbstractBaseUser (class): Django abstract base class for defining
+        a user
+        PermissionsMixin (class): Django out-the-box permission settings user
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
